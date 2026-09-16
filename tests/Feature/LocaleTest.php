e31502107d3bf998cf2a3124108165421cb3aa00
Umayantha <umayantha93@gmail.com>
2026-09-16 21:28:38 +0530

    add language

--- tests/Feature/LocaleTest.php
+++ tests/Feature/LocaleTest.php
@@ -14,7 +14,10 @@ class LocaleTest extends TestCase
             ->assertOk()
             ->assertJsonPath('locale', 'en')
             ->assertJsonPath('messages.shell.sign_out', 'Sign out')
-            ->assertJsonPath('messages.nav.Overview', 'Overview');
+            ->assertJsonPath('messages.nav.Overview', 'Overview')
+            ->assertJsonPath('messages.admit.title_vehicle', 'Admit a vehicle')
+            ->assertJsonPath('messages.pos.complete_sale', 'Complete sale')
+            ->assertJsonPath('messages.bill.add_to_bill', 'Add to bill');
     }
 
     public function test_translations_endpoint_returns_sinhala_ui_messages(): void
@@ -23,7 +26,10 @@ class LocaleTest extends TestCase
             ->assertOk()
             ->assertJsonPath('locale', 'si')
             ->assertJsonPath('messages.shell.sign_out', 'ඉවත් වන්න')
-            ->assertJsonPath('messages.nav.Overview', 'දළ විශ්ලේෂණය');
+            ->assertJsonPath('messages.nav.Overview', 'දළ විශ්ලේෂණය')
+            ->assertJsonPath('messages.admit.title_vehicle', 'වාහනය ඇතුළත් කරන්න')
+            ->assertJsonPath('messages.pos.complete_sale', 'විකුණුම අවසන් කරන්න')
+            ->assertJsonPath('messages.bill.add_to_bill', 'බිල්පතට එක් කරන්න');
     }
 
     public function test_authenticated_user_can_persist_locale(): void
